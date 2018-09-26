5684d4ad4fabd52b53abb03566e739c3cbfd5ff5
Ponkin Dmitrij <ponkin.dmitrij@gmail.com>
2018-09-26 08:45:48 +0300

    Adds findDuplicates function

--- task-3/js/main.js
+++ task-3/js/main.js
@@ -13,3 +13,21 @@ function sortWords(srcString) {
 
 // const str = 'ф-ция получает на вход строку со словами слова разделяются пробелами';
 // console.log( sortWords(str) );
+
+
+
+// ==================== findDuplicates ====================
+function findDuplicates(array) {
+  const res = [];
+
+  array.forEach(function(item, i, arr) {
+    if (arr.indexOf(item) !== i && res.indexOf(item) < 0) {
+      res.push(item);
+    }
+  })
+
+  return res;
+}
+
+// const arr2 = ['one', true, 2, 3, 4, 4, 4, 5, 6, 7, 7, 7, 'two', 'one', true];
+// console.log( findDuplicates(arr2) );
